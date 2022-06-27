d8cd9852a49cf3faaace18084d96970ce8222b48
superestze <lizh285@midea.com>
2022-06-27 13:56:08 +0800

    增加 sql 依赖

--- src/main/java/com/jiawa/wiki/controller/TextController.java
+++ src/main/java/com/jiawa/wiki/controller/TextController.java
@@ -1,11 +1,18 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class TextController {
+    @Resource
+    private TestService testService;
 
     @Value("${test.hello}")
     private String hello;
@@ -17,5 +24,10 @@ public class TextController {
     @RequestMapping("/hello")
     public String hello() {
         return this.hello;
+    }
+
+    @RequestMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }

6e8f52f3c6a97c17fbf5fbdb4a24025babbcbbda
superestze <lizh285@midea.com>
2022-06-27 11:04:11 +0800

    集成热部署

--- src/main/java/com/jiawa/wiki/controller/TextController.java
+++ src/main/java/com/jiawa/wiki/controller/TextController.java
@@ -1,10 +1,14 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TextController {
+
+    @Value("${test.hello}")
+    private String hello;
 
     /**
      * GET POST PUT DELETE
@@ -12,6 +16,6 @@ public class TextController {
      */
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello world";
+        return this.hello;
     }
 }

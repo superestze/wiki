ecb9266ffdfb6266884cd944ccd8a9354e4a3d4b
superestze <lizh285@midea.com>
2022-06-29 14:37:01 +0800

    feat: 配置 axios 拦截器

--- web/src/main.ts
+++ web/src/main.ts
@@ -9,6 +9,20 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER
 
+
+axios.interceptors.request.use((config) => {
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+})
+
+axios.interceptors.response.use((res) => {
+    return res;
+}, error => {
+    return Promise.reject(error);
+})
+
+
 const app = createApp(App);
 app.use(store).use(Antd).use(router).mount('#app')
 const icons: any = Icons;

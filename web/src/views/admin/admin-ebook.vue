638688d85bbbd80bd575d260d761dac365e03e72
superestze <lizh285@midea.com>
2022-07-01 00:09:16 +0800

    feat: 点击编辑按钮时， 点击编辑某一行的时候， 可以将该行的数据显示到表单

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,6 +1,5 @@
 <template>
   <a-layout>
-    11
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
@@ -17,7 +16,7 @@
 
         <template v-slot:action="{ text, record }">
           <a-space type="small">
-            <a-button type="primary" @click="edit"> 编辑</a-button>
+            <a-button type="primary" @click="edit(record)"> 编辑</a-button>
           </a-space>
           <a-button type="danger"> 删除</a-button>
         </template>
@@ -32,7 +31,24 @@
       title="电子书表单"
       :confirm-loading="modalLoading"
       @ok="handleModalOk">
-    <p>test</p>
+    <a-form :model="ebook" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover"/>
+      </a-form-item>
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name"/>
+      </a-form-item>
+      <a-form-item label="分类">
+        <a-cascader
+            v-model:value="categoryIds"
+            :field-names="{ label: 'name', value: 'id', children: 'children' }"
+            :options="level1"
+        />
+      </a-form-item>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.description" type="textarea"/>
+      </a-form-item>
+    </a-form>
 
   </a-modal>
 </template>
@@ -45,6 +61,7 @@ import axios from "axios";
 export default defineComponent({
   name: 'AdminEbook',
   setup() {
+    const ebook = ref()
     const modalVisible = ref(false)
     const modalLoading = ref(false)
 
@@ -56,9 +73,11 @@ export default defineComponent({
       }, 2000)
     }
 
-    const edit = () => {
+    const edit = (record: any) => {
       modalVisible.value = true
+      ebook.value = record
     }
+
     const ebooks = ref()
     const pagination = ref({
       current: 1,
@@ -136,7 +155,8 @@ export default defineComponent({
       edit,
       modalVisible,
       modalLoading,
-      handleModalOk
+      handleModalOk,
+      ebook
     }
   }
 })

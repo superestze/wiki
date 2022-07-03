c326ecb233c7b156ad5cb481ebd42e42dd6ed2d2
superestze <lizh285@midea.com>
2022-07-03 23:20:58 +0800

    feat: 前端增加登录功能

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,27 +1,43 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo"/>
+    <div class="logo">甲蛙知识库</div>
     <a-menu
         theme="dark"
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="/home">
+      <a-menu-item key="/">
         <router-link to="/">首页</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/user">
+      <a-menu-item key="/admin/user" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/user">用户管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/ebook">
-        <router-link to="/admin/ebook">分类管理</router-link>
+      <a-menu-item key="/admin/ebook" :style="user.id? {} : {display:'none'}">
+        <router-link to="/admin/ebook">电子书管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/category">
+      <a-menu-item key="/admin/category" :style="user.id? {} : {display:'none'}">
         <router-link to="/admin/category">分类管理</router-link>
       </a-menu-item>
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
+      <a-menu-item key="/aliyun">
+        <router-link to="/aliyun">阿里云优惠</router-link>
+      </a-menu-item>
+      <a-popconfirm
+          title="确认退出登录?"
+          ok-text="是"
+          cancel-text="否"
+          @confirm="logout()"
+      >
+        <a class="login-menu" v-show="user.id">
+          <span>退出登录</span>
+        </a>
+      </a-popconfirm>
+      <a class="login-menu" v-show="user.id">
+        <span>您好：{{ user.name }}</span>
+      </a>
+      <a class="login-menu" v-show="!user.id" @click="showLoginModal">
         <span>登录</span>
       </a>
     </a-menu>
@@ -43,10 +59,12 @@
     </a-modal>
   </a-layout-header>
 </template>
+
 <script lang="ts">
-import {computed, defineComponent, ref} from 'vue';
-import axios from "axios";
-import {message} from "ant-design-vue";
+import {defineComponent, ref, computed} from 'vue';
+import axios from 'axios';
+import {message} from 'ant-design-vue';
+import store from "@/store";
 
 declare let hexMd5: any;
 declare let KEY: any;
@@ -113,9 +131,21 @@ export default defineComponent({
   }
 });
 </script>
+
 <style>
+.logo {
+  width: 120px;
+  height: 31px;
+  /*background: rgba(255, 255, 255, 0.2);*/
+  /*margin: 16px 28px 16px 0;*/
+  float: left;
+  color: white;
+  font-size: 18px;
+}
+
 .login-menu {
   float: right;
   color: white;
+  padding-left: 10px;
 }
 </style>

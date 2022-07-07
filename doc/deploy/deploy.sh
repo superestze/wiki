14493d3c6ae1ed60fa45f6c3604b065f9b0e2bcf
superestze <lizh285@midea.com>
2022-07-08 07:54:23 +0800

    feat: 发布部署后端代码

---
+++ doc/deploy/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "publish ------"
+
+process_id=`ps -ef | grep wiki.jar | grep -v grep|awk  '{print $2}'`
+if [$process_id]; then
+  sudo kill -9 $process_id
+fi
+
+source  /etc/profile
+nohup java -jar -Dspring.profile.active=prod ~/wiki/wiki.jar > /dev/null 2>&1 &
+
+
+echo "end publish"
